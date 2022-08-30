002de1121e882482415d722a42cb6d1dad4989d3
SoniPratham <d21it173@charusat.edu.in>
2022-08-30 13:39:18 +0530

    Added security with multiple users in memory authentication.

--- src/main/java/com/example/demo/config/SecurityConfigClass.java
+++ src/main/java/com/example/demo/config/SecurityConfigClass.java
@@ -1,4 +1,28 @@
 package com.example.demo.config;
 
-public class SecurityConfigClass {
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfigClass extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("PythonSoni")
+                .password("admin")
+                .roles("Admin")
+                .and()
+                .withUser("Soni")
+                .password("kemchho")
+                .roles("user");
+    }
+
+    @Bean
+    public PasswordEncoder getPassWordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
